7dc0d0726c6a430bd100282182ee54044c85b780
Praveen Ramachandran <praveen.thayikkattil@gmail.com>
2020-08-06 11:02:06 +0530

    Ininitial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Calendar from "./components/Calendar";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="logo">
+          <span className="icon">date_range</span>
+
+          <span>
+            react <strong>calendar</strong>
+          </span>
+        </div>
       </header>
+
+      <main>
+        <Calendar />
+      </main>
     </div>
   );
 }
